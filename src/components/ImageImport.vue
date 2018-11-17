0dea4977d3b637948f6b238b05bf82429ee86b0b
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-11-17 22:50:24 +0100

    Added files select.

--- src/components/ImageImport.vue
+++ src/components/ImageImport.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
 	v-card.imageImport
-		label Select the images
-			input(type="file" multiple)
+		v-container
+			label Select the images
+				input(type="file" multiple accept="image/*" @change="imagesSelected")
+			v-divider
+			div Selected files: {{selectedFiles}}
+
 </template>
 
 <script lang="ts">
 	import { Component, Vue } from "vue-property-decorator"
 	@Component({components: {}})
 	export default class ImageImport extends Vue {
+		public selectedFiles = 0
 
+		public imagesSelected(event: Event): void {
+			const files = this.getFilesFromInput(event.target as HTMLInputElement)
+
+			this.selectedFiles = files.length
+		}
+
+		private getFilesFromInput(input: HTMLInputElement): File[] {
+			if (input.files === null || input.files.length === 0)
+				return []
+
+			const files = Array.from(input.files)
+			input.value = ""
+
+			return files
+		}
 	}
 </script>
 
