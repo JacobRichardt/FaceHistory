b72e93038c88664579ccb97e5a826a0c289a5c5a
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-11-17 21:51:19 +0100

    Updated template files.

---
+++ src/components/ImageImport.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="imageImport">
+		<label>
+			Select files
+			<input type="file">
+		</label>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator"
+	@Component({components: {}})
+	export default class ImageImport extends Vue {
+
+	}
+</script>
+
+<style scoped type="stylus">
+
+</style>
