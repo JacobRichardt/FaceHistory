553568a8f2ee75fa1db5c5e1cda680b2191187ae
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-11-18 02:37:35 +0100

    Added video rendering.

--- src/components/ImageImport.vue
+++ src/components/ImageImport.vue
@@ -4,20 +4,83 @@
 			label Select the images
 				input(type="file" multiple accept="image/*" @change="imagesSelected")
 			v-divider
-			div Selected files: {{selectedFiles}}
+			div Selected files: {{selectedNumberOfFiles}}
+			video(v-if="videoUrl" :src="videoUrl" controls)
 
 </template>
 
 <script lang="ts">
 	import { Component, Vue } from "vue-property-decorator"
+
+	declare var MediaRecorder: any
+
 	@Component({components: {}})
 	export default class ImageImport extends Vue {
-		public selectedFiles = 0
+		public selectedNumberOfFiles = 0
+		public videoUrl: string | null = null
 
 		public imagesSelected(event: Event): void {
 			const files = this.getFilesFromInput(event.target as HTMLInputElement)
 
-			this.selectedFiles = files.length
+			this.selectedNumberOfFiles = files.length
+
+			if (files.length !== 0)
+				this.createImageBitmaps(files)
+					.then(images => this.convertToVideo(images))
+					.then(blob => {
+						if (this.videoUrl !== null)
+							URL.revokeObjectURL(this.videoUrl)
+
+						this.videoUrl = URL.createObjectURL(blob)
+					})
+		}
+
+		private createImageBitmaps(files: File[]): Promise<ImageBitmap[]> {
+			return Promise.all(files.map(f => createImageBitmap(f, 0, 0, 1920, 1080)))
+		}
+
+		private convertToVideo(images: ImageBitmap[]): Promise<Blob> {
+			const canvas = document.createElement("Canvas") as HTMLCanvasElement
+			const width = 1920
+			const height = 1080
+
+			canvas.setAttribute("width", width.toString(10))
+			canvas.setAttribute("height", height.toString(10))
+			/*canvas.style.position = "absolute"
+			canvas.style.top = "0"
+			canvas.style.opacity = "0"*/
+			document.body.appendChild(canvas)
+			const context = canvas.getContext("2d")!
+
+			const stream = (canvas as any).captureStream(60)
+			const recorder = new MediaRecorder(stream)
+			const chunks: BlobPart[] = []
+			let imageIndex = 0
+
+			recorder.ondataavailable = (event: any) => {
+				chunks.push(event.data)
+			}
+
+			const animate = () => {
+				context.fillStyle = "black"
+				context.fillRect(0, 0, width, height)
+				context.drawImage(images[imageIndex++], 0, 0)
+
+				if (imageIndex < images.length)
+					requestAnimationFrame(animate)
+				else
+					recorder.stop()
+			}
+
+			return new Promise<Blob>(resolve => {
+				recorder.onstop = () => {
+					document.body.removeChild(canvas)
+					resolve(new Blob(chunks, {type: "video/*"}))
+				}
+
+				recorder.start()
+				animate()
+			})
 		}
 
 		private getFilesFromInput(input: HTMLInputElement): File[] {
